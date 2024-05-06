59fa8244ceb6732289b6753edf4bdca1289d722f
gokulmano2003 <121616386+gokulmano2003@users.noreply.github.com>
2024-05-06 18:57:49 +0530

    Add files via upload

---
+++ Newfolder/logicalop.js
@@ -0,0 +1,14 @@
+var a=33
+
+
+switch(a){
+    case 23:{
+        console.log("first week of the day")
+        break;
+    }
+    case 33:{
+        console.log("second week of the day")
+        break;
+    }
+    default: console.log("please check your input")
+}
